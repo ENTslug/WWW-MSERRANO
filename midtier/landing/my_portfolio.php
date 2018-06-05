280d8a7d0d3c83662004e61619e88a258fa1c833
ENTslug <Mark.Serrano26@gmail.com>
2018-06-05 12:48:07 -0700

    + create the sanitizer library. add page_sanitizer. add cst_sanitizer. + add sanitizer_core, sanitizer_value, sanitizer_config, sanitizer_manager. + my_portfolio will handle different input cases.

--- midtier/landing/my_portfolio.php
+++ midtier/landing/my_portfolio.php
@@ -2,14 +2,27 @@
 
 class my_portfolio extends page_base {
 
-    public function build(output $output) {
-        $sanitized    = json_decode($this->post, true); //TODO: sanitizer
-        $has_access   = $sanitized[cst_portfolio::p_has_access];
-        $list_project = $this->retrieve_projects();
+    use page_sanitizer;
 
-        $continue = true;
-        $continue = $continue && ($has_access === true);
-        $continue = $continue && (empty($list_project) === false);
+    public function __destruct() {
+        $this->destruct_page_sanitizer();
+    }
+
+    public function build(output $output) {
+        $options = new sanitizer_config();
+        $options->register_set('post', $this->post, cst_sanitizer::type_json);
+
+        $this->_build_sanitizer($output, $options);
+    }
+
+    public function build_sanitizer(output $output) {
+        $this->sanitizer->queue_item('post', cst_portfolio::p_has_access, cst_sanitizer::sane_boolean);
+        $continue = $this->sanitizer->run_it();
+
+        if ($continue === true) {
+            $has_access   = $this->sanitizer->get('post', cst_portfolio::p_has_access);
+            $list_project = $this->retrieve_projects($has_access);
+        }
 
         if ($continue === true) {
             $this->response[cst_portfolio::p_list_project] = $list_project;
@@ -19,29 +32,32 @@ class my_portfolio extends page_base {
     }
 
     // --- helpers ---
-    private function retrieve_projects() {
-        $return = array(
-            // DevOps
-            array(
-                cst_portfolio::p_title => 'DevOps',
-                cst_portfolio::p_thumb => '',
-            ),
-            // LAB-MSERRANO
-            array(
-                cst_portfolio::p_title => 'lab.mserrano.net',
-                cst_portfolio::p_thumb => '',
-            ),
-            // WWW-MSERRANO
-            array(
-                cst_portfolio::p_title => 'www.mserrano.net',
-                cst_portfolio::p_thumb => '',
-            ),
-            // DOCS-MSERRANO
-            array(
-                cst_portfolio::p_title => 'docs.mserrano.net',
-                cst_portfolio::p_thumb => '',
-            ),
-        );
+    private function retrieve_projects(bool $has_access): array {
+        $return = array();
+        if ($has_access === true) {
+            $return = array(
+                // DevOps
+                array(
+                    cst_portfolio::p_title => 'DevOps',
+                    cst_portfolio::p_thumb => '',
+                ),
+                // LAB-MSERRANO
+                array(
+                    cst_portfolio::p_title => 'lab.mserrano.net',
+                    cst_portfolio::p_thumb => '',
+                ),
+                // WWW-MSERRANO
+                array(
+                    cst_portfolio::p_title => 'www.mserrano.net',
+                    cst_portfolio::p_thumb => '',
+                ),
+                // DOCS-MSERRANO
+                array(
+                    cst_portfolio::p_title => 'docs.mserrano.net',
+                    cst_portfolio::p_thumb => '',
+                ),
+            );
+        }
         return $return;
     }
 
