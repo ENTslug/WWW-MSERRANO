49742bb65b875dd4d9e78ac2744234cfdffbb1e2
ENTslug <Mark.Serrano26@gmail.com>
2018-06-09 12:56:27 -0700

    + add feather. set config block to define svg icons to bundle together. + set config block to define color palettes. define scw_blue, scw_yellow, and a warning palette. + add pdf, linkedin, and github icon to toolbar.

---
+++ frontend/src/blocks/config__icons.js
@@ -0,0 +1,7 @@
+angular.module("www.blocks")
+        .config(function ($mdIconProvider) {
+            $mdIconProvider
+                    .iconSet('social:github', 'resources/feather/github.svg', 24)
+                    .iconSet('social:linkedin', 'resources/feather/linkedin.svg', 24)
+                    .iconSet('social:resume', 'resources/feather/file-text.svg', 24)
+        });
